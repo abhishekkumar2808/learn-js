a685aed37ebdd6027da180857e27baf4e1f4707f
abhishekkumar <kumar.abhishe@northeastern.edu>
2024-02-02 16:43:53 -0500

    css files

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -10,8 +10,9 @@ function createTRNode(colNodes) {
   return trNode;
 }
 
-function createTDNode(childNode) {
+function createTDNode(childNode, id) {
   let tdNode = document.createElement("td");
+  tdNode.id = id;
   tdNode.appendChild(childNode);
   return tdNode;
 }
@@ -21,11 +22,28 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function handlleEditButtonClick(index){
+
+  let cell = document.getElementById(`col-${index}`);
+  cell.innerHTML = ' <input type="text" placeholder="enter (x,y)"></input>'
+
+}
+
+function createButtonNode(text, index = ''){
+
+  let buttonNode = document.createElement("button");
+  buttonNode.innerText = text;
+
+  buttonNode.onclick = () => {handlleEditButtonClick(index);}
+  return buttonNode;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"), `col-${i}`);
+    let col2 = createButtonNode("Edit Text", i);
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
